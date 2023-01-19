63d8e6d95347546ee77faf0b8418ad6d3d5596df
BrandonInholland <42965932+BrandonInholland@users.noreply.github.com>
2023-01-19 14:23:50 +0100

    User Creation Vue

---
+++ src/components/users/CreateUser.vue
@@ -0,0 +1,94 @@
+<template>
+  <section>
+    <div class="container">
+      <form ref="form">
+        <h2 class="mt-3 mt-lg-5">Create a user</h2>
+        <h5 class="mb-4"></h5>
+
+        <div class="input-group mb-3">
+          <span class="input-group-text">User Name</span>
+          <input type="text" class="form-control" v-model="user.username" />
+        </div>
+
+        <div class="input-group mb-3">
+          <span class="input-group-text">User Email</span>
+          <textarea
+            class="form-control"
+            v-model="user.email"
+          ></textarea>
+        </div>
+        <div class="input-group mb-3">
+          <span class="input-group-text">Role</span>
+          <textarea
+            class="form-control"
+            v-model="user.role"
+          ></textarea>
+        </div>
+        <div class="input-group mb-3">
+          <span class="input-group-text">Picture</span>
+          <textarea
+            class="form-control"
+            v-model="user.picture"
+          ></textarea>
+        </div>        
+
+        <div class="input-group mt-4">
+          <button type="button" class="btn btn-primary" @click="addUser">
+            Create User
+          </button>
+          <button
+            type="button"
+            class="btn btn-danger"
+            @click="this.$router.push('/users')"
+          >
+            Cancel
+          </button>
+        </div>
+      </form>
+    </div>
+  </section>
+</template>
+
+<script>
+import axios from '../../axios-auth'
+
+export default {
+  name: "CreateUser",
+  data() {
+    return {
+      user: {
+        Name: "",
+        email: "",
+        role:"",
+        picture: "",
+      },
+    };
+  },
+  methods: {
+    addUser() {
+
+      axios
+        .post("https://cardisc.azurewebsites.net/api/users", this.user
+        )
+        .then((res) => {
+          console.log(res.data);
+          this.$refs.form.reset();
+          this.$router.push("/users");
+        })
+        .catch((error) => console.log(error));
+    },
+  },
+  mounted() {
+    // axios
+    //   .get("http://localhost/categories")
+    //   .then((result) => {
+    //     console.log(result);
+    //     this.categories = result.data;
+    //   })
+    //   .catch((error) => console.log(error));
+  },
+};
+</script>
+
+<style>
+</style>
--- src/components/users/UserList.vue
+++ src/components/users/UserList.vue
@@ -2,7 +2,7 @@
     <section>
       <div class="container">
         <h2 class="mt-3 mt-lg-5">User</h2>
-          <button type="button" class="btn btn-primary mt-3" @click="this.$router.push('/createproduct');">
+          <button type="button" class="btn btn-primary mt-3" @click="this.$router.push('/createuser');">
               Add Users
             </button>
         <div class="row mt-3">
