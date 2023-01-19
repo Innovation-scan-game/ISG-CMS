b47484209ba5f4cdb591a0c2f54381f6d59bd76a
BrandonInholland <42965932+BrandonInholland@users.noreply.github.com>
2023-01-19 15:51:34 +0100

    Create EditUser.vue

---
+++ src/components/users/EditUser.vue
@@ -0,0 +1,78 @@
+<template>
+  <section>
+    <div class="container">
+      <form ref="form">
+        <h2 class="mt-3 mt-lg-5">Edit User</h2>
+        <h5 class="mb-4"></h5>
+
+        <div class="input-group mb-3">
+          <span class="input-group-text">username</span>
+          <input type="text" class="form-control" v-model="user.username" />
+        </div>
+
+        <div class="input-group mb-3">
+          <span class="input-group-text">User Email</span>
+          <textarea
+            class="form-control"
+            v-model="user.email"
+          ></textarea>
+        </div>
+        <div class="input-group mb-3">
+          <span class="input-group-text">Role</span>
+          <textarea
+            class="form-control"
+            v-model="user.role"
+          ></textarea>
+        </div>
+
+        <div class="input-group mt-4">
+          <button type="button" class="btn btn-primary" @click="updateUser">
+            Save changes
+          </button>
+          <button
+            type="button"
+            class="btn btn-danger"
+            @click="this.$router.push('/users')"
+          >
+            Cancel
+          </button>
+        </div>
+      </form>
+    </div>
+  </section>
+</template>
+
+<script>
+import axios from '../../axios-auth'
+
+export default {
+  name: "CreateUser",
+  props: {
+    id: Number,
+  },
+  data() {
+    return {
+      user: {
+        username: "",
+        email: "",
+        role: "",
+      },
+    };
+  },
+  methods: {
+    updateUser() {
+      axios
+        .put("https://cardisc.azurewebsites.net/api/users/" + this.user.id)
+        .then((res) => {
+          console.log(res.data);
+          this.$refs.form.reset();
+          this.$router.push("/users");
+        })
+        .catch((error) => console.log(error));
+    },
+  },
+};
+</script>
+
+<style>
+</style>
