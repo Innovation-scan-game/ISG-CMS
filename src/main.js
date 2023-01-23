c93b159b015458dedf5011bb7bb544cd2e65e8b7
John <johnbond@live.nl>
2023-01-23 17:01:41 +0100

    trying something

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import cors from 'cors'
 
 import App from './App.vue'
 import router from './router'
@@ -8,7 +7,6 @@ import router from './router'
 import './assets/main.css'
 
 const app = createApp(App)
-app.use(cors())
 
 app.use(createPinia())
 app.use(router)
--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,17 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { viteSingleFile } from "vite-plugin-singlefile"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), viteSingleFile()],
+  devServer: {
+    proxy: 'https://salmon-stone-0f9b3d703.2.azurestaticapps.net/',
+    },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
