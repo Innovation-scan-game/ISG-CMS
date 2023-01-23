c3d9252d0e296ba2a43cddb7ff19cb78bdf26507
BrandonInholland <42965932+BrandonInholland@users.noreply.github.com>
2023-01-23 15:44:10 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '../components/Home.vue';
-import ProductList from '../components/products/ProductList.vue';
-import CreateProduct from '../components/products/CreateProduct.vue';
-import EditProduct from '../components/products/EditProduct.vue';
+import CardList from '../components/cards/CardList.vue';
+import CreateCard from '../components/cards/CreateCard.vue';
+import EditCard from '../components/cards/EditCard.vue';
 import UserList from '../components/users/UserList.vue';
 import CreateUser from '../components/users/CreateUser.vue';
 import EditUser from '../components/users/EditUser.vue';
@@ -13,12 +13,12 @@ import Login from '../components/Login.vue';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', component: Home },
-    { path: '/products', component: ProductList },
-    { path: '/users', component: UserList },
-    { path: '/login', component: Login },
-    { path: '/createproduct', component: CreateProduct },
-    { path: '/editproduct/:id', component: EditProduct, props: true  },
+    { path: '/', component: Home , meta: {requiresAuth: true}},
+    { path: '/cards', component: CardList , meta: {requiresAuth: true}},
+    { path: '/login', component: Login , name: 'Login'},
+    { path: '/createcard', component: CreateCard , meta: {requiresAuth: true}},
+    { path: '/editcard/:id', component: EditCard, props: true  , meta: {requiresAuth: true}},
+    { path: '/users', component: UserList },     
     { path: '/createuser', component: CreateUser },
     { path: '/edituser/:id', component: EditUser, props: true  },
   ]
